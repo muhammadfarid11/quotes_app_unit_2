089f49a2bb10d0f429763654b2455798629e82e8
Muhammad Farid <gantengfarid5@gmail.com>
2023-06-09 17:06:27 +0700

    update

---
+++ lib/repositories/quotes_repository.dart
@@ -0,0 +1,31 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:quotes_app_unit_2/models/quote_api_model.dart';
+
+class QuotesRepository {
+  Future<List<QuoteApi>> getRandomQuotes() async {
+    String url = "https://api.quotable.io";
+    String endpoint = "/quotes/random?limit=20";
+
+    final response = await http.get(Uri.parse(url + endpoint));
+    
+    // check if error
+    if (response.statusCode != 200) {
+      throw "Ada error";
+    }
+
+    // if success
+    List dataJson = jsonDecode(response.body);
+    print(dataJson);
+
+    List<QuoteApi> quotes = [];
+
+    for (var item in dataJson) {
+      QuoteApi quote = QuoteApi.fromJson(item);
+      quotes.add(quote);
+    }
+
+    return quotes;
+  }
+}
